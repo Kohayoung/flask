dbf6c8f6c63cded17c9722749667d04cb5801b5c
Ko Hayoung <freekhy92@gmail.com>
2019-12-12 13:40:59 +0900

    for, if문 사용

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask, escape, request, render_template
-
+import random
 app = Flask(__name__)
 
 @app.route('/')
@@ -40,6 +40,40 @@ def calculate(num):
     #def_cal_num = num*num*num
     def_cal_num = num**3
     return render_template('calculate.html', html_num = def_num, html_cal_num = def_cal_num)
+
+@app.route('/lunch')
+def lunch():
+    
+    #lunch = ['20층','김밥','자장면','부대찌개','돈까스']
+    #lunch_image = ['http://cfile216.uf.daum.net/image/99B8464E5B3DAF2219F22E'
+    #            ,'http://recipe1.ezmember.co.kr/cache/recipe/2016/11/28/6bc7f3c7a3fdf517e6943dd14a9b3df01.jpg'
+    #            ,'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F236F694453AA52A70D'
+    #            ,'http://foodyap.co.kr/shopimages/goldplate1/049001000008.jpg?1561711447'
+    #            ,'https://imagescdn.gettyimagesbank.com/500/201811/a11240496.jpg']
+    #index = random.randrange(0,len(lunch))
+    #return render_template('lunch.html', food= lunch[index], food_image = lunch_image[index])
+
+    menus = {
+        "20층":"http://cfile216.uf.daum.net/image/99B8464E5B3DAF2219F22E"
+        ,"김밥":"http://recipe1.ezmember.co.kr/cache/recipe/2016/11/28/6bc7f3c7a3fdf517e6943dd14a9b3df01.jpg"
+        ,"자장면":"https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F236F694453AA52A70D"
+        ,"부대찌개":"http://foodyap.co.kr/shopimages/goldplate1/049001000008.jpg?1561711447"
+        ,"돈까스":"https://imagescdn.gettyimagesbank.com/500/201811/a11240496.jpg"
+        }
+
+    menu_list = list(menus.keys())
+    pick = random.choice(menu_list)
+    img =menus[pick]
+    print(menu_list)
+
+    return render_template('lunch.html', food=pick, food_image=img)
+
+@app.route('/movies')
+def movies():
+    movies = ['겨울왕국2', '굿라이어', '주만지']
+    return render_template('movies.html', movies = movies)
+
+
 
 
 if __name__ =='__main__':
