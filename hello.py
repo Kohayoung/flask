790ed8e133eb6489ccfd090e83c72d5fccb7a2d8
Ko Hayoung <freekhy92@gmail.com>
2019-12-12 11:17:11 +0900

    동적라우팅 사용해보기

--- hello.py
+++ hello.py
@@ -29,7 +29,17 @@ def variable():
     name = "고하영이"
     return render_template('variable.html', html_name = name)
 
+@app.route('/greeting/<string:name>/')
+def greeting(name):
+    def_name = name
+    return render_template('greeting.html', html_name = def_name)
 
+@app.route('/cube/<int:num>/')
+def calculate(num):
+    def_num = num
+    #def_cal_num = num*num*num
+    def_cal_num = num**3
+    return render_template('calculate.html', html_num = def_num, html_cal_num = def_cal_num)
 
 
 if __name__ =='__main__':
